59bc8ad81644f11300413aff23afd0643feec197
Diego <dcsuperdiego2@gmail.com>
2025-10-23 09:35:56 -0600

    Add files via upload

--- Desktop/Patitas_Felices_API/app.js
+++ Desktop/Patitas_Felices_API/app.js
@@ -1,8 +1,8 @@
-require('dotenv').config();  // Cargar las variables de entorno desde el archivo .env
+require('dotenv').config();  
 const express = require('express');
 const cors = require('cors');
 
-// Importar las rutas de las diferentes tablas
+
 const mascotasRoutes = require('./routes/mascotasRoutes');
 const duenosRoutes = require('./routes/duenosRoutes');
 const citasRoutes = require('./routes/citasRoutes');
@@ -10,41 +10,41 @@ const medicosRoutes = require('./routes/medicosRoutes');
 const tratamientosRoutes = require('./routes/tratamientosRoutes');
 const fichasRoutes = require('./routes/fichasRoutes');
 
-// Crear una nueva instancia de Express
+
 const app = express();
 
-// Configurar el puerto de la aplicación (Render lo proporcionará a través de process.env.PORT)
-const PORT = process.env.PORT || 10000;  // Usar el puerto proporcionado por Render o 10000 por defecto
 
-// Middleware para habilitar CORS y manejar solicitudes JSON
+const PORT = process.env.PORT || 10000;  
+
+
 app.use(cors());
 app.use(express.json());
 
 // Usar las rutas importadas
-app.use('/api/mascotas', mascotasRoutes);        // Rutas para las mascotas
-app.use('/api/duenos', duenosRoutes);            // Rutas para los dueños
-app.use('/api/citas', citasRoutes);              // Rutas para las citas
-app.use('/api/medicos', medicosRoutes);          // Rutas para los médicos
-app.use('/api/tratamientos', tratamientosRoutes); // Rutas para los tratamientos
-app.use('/api/fichas', fichasRoutes);            // Rutas para las fichas
+app.use('/api/mascotas', mascotasRoutes);        
+app.use('/api/duenos', duenosRoutes);            
+app.use('/api/citas', citasRoutes);              
+app.use('/api/medicos', medicosRoutes);          
+app.use('/api/tratamientos', tratamientosRoutes); 
+app.use('/api/fichas', fichasRoutes);            
 
-// Ruta de prueba para comprobar que la API está funcionando correctamente
+
 app.get('/', (req, res) => {
   res.send('✅ ¡API de Veterinaria funcionando correctamente!');
 });
 
-// Ruta para probar la conexión a la base de datos
+
 app.get('/ping-db', async (req, res) => {
   try {
-    const [r] = await pool.query('SELECT 1 AS ok');  // Realizar una consulta simple
-    res.json(r);  // Responder con el resultado de la consulta
+    const [r] = await pool.query('SELECT 1 AS ok');  
+    res.json(r);  
   } catch (err) {
     console.error('Error DB (ping):', err);
     res.status(500).send('No me pude conectar a MySQL');
   }
 });
 
-// Iniciar el servidor en el puerto configurado
+
 app.listen(PORT, () => {
   console.log(`🚀 Servidor API escuchando en http://localhost:${PORT}`);
 });
