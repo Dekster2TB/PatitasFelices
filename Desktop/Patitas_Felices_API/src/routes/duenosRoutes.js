c62d188556b7bb1ddd96b5df65f03791aa89d86f
Diego <dcsuperdiego2@gmail.com>
2025-10-23 18:33:21 -0600

    Add files via upload

--- Desktop/Patitas_Felices_API/src/routes/duenosRoutes.js
+++ Desktop/Patitas_Felices_API/src/routes/duenosRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const duenosController = require('../controllers/duenosController');  // Importar el controlador de dueños
+const duenosControllers = require('../controllers/duenosControllers');  // Importar el controlador de dueños
 
 // Definir las rutas CRUD para la tabla 'duenos'
-router.get('/', duenosController.obtenerTodos);  // Obtener todos los dueños
-router.get('/:id', duenosController.obtenerPorId);  // Obtener un dueño por ID
-router.post('/', duenosController.crear);  // Crear un nuevo dueño
-router.put('/:id', duenosController.actualizar);  // Actualizar los datos de un dueño
-router.delete('/:id', duenosController.eliminar);  // Eliminar un dueño
+router.get('/', duenosControllers.obtenerTodos);  // Obtener todos los dueños
+router.get('/:id', duenosControllers.obtenerPorId);  // Obtener un dueño por ID
+router.post('/', duenosControllers.crear);  // Crear un nuevo dueño
+router.put('/:id', duenosControllers.actualizar);  // Actualizar los datos de un dueño
+router.delete('/:id', duenosControllers.eliminar);  // Eliminar un dueño
 
 module.exports = router;  // Exportar las rutas para ser utilizadas en app.js
